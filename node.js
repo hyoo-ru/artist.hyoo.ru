07a6ee222e38d55de80a1216a91c9d27c0fcd2e9
nin-jin <nin-jin@users.noreply.github.com>
2023-03-20 07:15:10 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -7542,7 +7542,12 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_offline();
+    try {
+        $mol_offline();
+    }
+    catch (error) {
+        console.error(error);
+    }
 })($ || ($ = {}));
 //mol/offline/install/install.ts
 ;

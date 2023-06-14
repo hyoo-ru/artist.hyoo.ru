80d63f8693d61376107fb8d88091645f1dff05d2
jin <jin@hyoo.ru>
2023-06-14 20:05:38 +0300

    Port to photoreal

--- imagine/imagine.ts
+++ imagine/imagine.ts
@@ -1,51 +1,54 @@
 namespace $ {
 	
-	// export function $hyoo_artist_imagine( this: $, prompt: string ) {
+	// export function $hyoo_artist_imagine( this: $, prompt: string, forbid = '' ) {
+	// 	if( !prompt ) return ''
 	// 	return this.$mol_huggingface_run( 'hyoo-imagine', 0, prompt )[0]
 	// }
 	
-	// export function $hyoo_artist_imagine( this: $, prompt: string, forbid?: string ) {
+	export function $hyoo_artist_imagine( this: $, prompt: string, forbid = '' ) {
 		
-	// 	const space = 'artgan-stable-diffusion-controlnet-webu-185266f'
-
-	// 	const path = this.$mol_huggingface_run(
-	// 		space,
-	// 		0,
-	// 		'dreamlike-art/dreamlike-photoreal-2.0',
-	// 		prompt,
-	// 		forbid,
-	// 		1, // Images
-	// 		"DDIM",
-	// 		7.5, // Guidance
-	// 		70, // Steps
-	// 		512, // Width
-	// 		512, // Height
-	// 		0,
-	// 	)[0][0].name as string
-
-	// 	return `https://${space}.hf.space/file=${path}`
-
-	// }
+	if( !prompt ) return ''
 	
-	export function $hyoo_artist_imagine(
-		this: $,
-		prompt: string,
-		forbid = '',
-	) {
-		
-		if( !prompt ) return ''
-		
-		const space = 'ai-forever-kandinsky2-1'
-		
+	const space = 'artgan-stable-diffusion-controlnet-webui'
+
 		const path = this.$mol_huggingface_run(
 			space,
-			2,
+			0,
+			'dreamlike-art/dreamlike-photoreal-2.0',
 			prompt,
 			forbid,
+			1, // Images
+			"DDIM",
+			7.5, // Guidance
+			40, // Steps
+			512, // Width
+			512, // Height
+			0,
 		)[0][0].name as string
 
 		return `https://${space}.hf.space/file=${path}`
 
 	}
+	
+	// export function $hyoo_artist_imagine(
+	// 	this: $,
+	// 	prompt: string,
+	// 	forbid = '',
+	// ) {
+		
+	// 	if( !prompt ) return ''
+		
+	// 	const space = 'ai-forever-kandinsky2-1'
+		
+	// 	const path = this.$mol_huggingface_run(
+	// 		space,
+	// 		2,
+	// 		prompt,
+	// 		forbid,
+	// 	)[0][0].name as string
+
+	// 	return `https://${space}.hf.space/file=${path}`
+
+	// }
 	
 }

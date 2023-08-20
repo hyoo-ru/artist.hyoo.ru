23900f975b0acd3941367b6f6f505da4baaf9b0d
jin <jin@hyoo.ru>
2023-08-20 22:31:19 +0300

    Use our api

--- imagine/imagine.ts
+++ imagine/imagine.ts
@@ -1,34 +1,35 @@
 namespace $ {
 	
-	// export function $hyoo_artist_imagine( this: $, prompt: string, forbid = '' ) {
-	// 	if( !prompt ) return ''
-	// 	return this.$mol_huggingface_run( 'hyoo-imagine', 0, prompt )[0]
-	// }
-	
 	export function $hyoo_artist_imagine( this: $, prompt: string, forbid = '' ) {
-		
-	if( !prompt ) return ''
-	
-	const space = 'artgan-stable-diffusion-controlnet-webui'
-
-		const path = this.$mol_huggingface_run(
-			space,
-			0,
-			'dreamlike-art/dreamlike-photoreal-2.0',
-			prompt,
-			forbid,
-			1, // Images
-			"DDIM",
-			7.5, // Guidance
-			40, // Steps
-			512, // Width
-			512, // Height
-			0,
-		)[0][0].name as string
-
-		return `https://${space}.hf.space/file=${path}`
-
+		if( !prompt ) return ''
+		return this.$mol_huggingface_run( 'hyoo-imagine', 0, prompt + ' ' + $mol_wire_sync( Math ).random() )[0]
 	}
+	
+	// export function $hyoo_artist_imagine( this: $, prompt: string, forbid = '' ) {
+		
+	// if( !prompt ) return ''
+	
+	// /** @see https://huggingface.co/spaces/ArtGAN/Image-Diffusion-WebUI */
+	// const space = 'artgan-image-diffusion-webui'
+
+	// 	const path = this.$mol_huggingface_run(
+	// 		space,
+	// 		0,
+	// 		'dreamlike-art/dreamlike-photoreal-2.0',
+	// 		prompt,
+	// 		forbid,
+	// 		1, // Images
+	// 		"DDIM",
+	// 		7.5, // Guidance
+	// 		40, // Steps
+	// 		512, // Width
+	// 		512, // Height
+	// 		0,
+	// 	)[0][0].name as string
+
+	// 	return `https://${space}.hf.space/file=${path}`
+
+	// }
 	
 	// export function $hyoo_artist_imagine(
 	// 	this: $,
